4295bad5e80a5177588b922e2523876ded20e363
wwydmanski <wwydmanski@gmail.com>
2019-09-15 06:59:08 +0200

    feat: first working version!

--- solver.py
+++ solver.py
@@ -17,15 +17,50 @@ class Solver:
         self.last_pos_2 = 0
 
         self.coeff = 1
+        self.coeff = 1
         self.coeff_decay = 0.7
         self.last_time = time.time()
         self.position = None
+        self.position_sum = 0
+        self.starting_position = 0
+        self.movement_angle = 0.25
+        self.inertia = 0.09
 
-    def zero(self, time, position):
+    def calibrate(self, tm, position):
+        print(tm, position)
+        if tm == 0:
+            self.starting_position = position
+            self.env.step(-1, False)
+        if tm >= 2 and self.starting_position+self.inertia> position > self.starting_position-self.inertia:
+            self.env.step(0, True)
+        if tm >= 3 and self.env.stopped:
+            print(f"Inertia degrees: {position - self.starting_position}")
+
+    def zeroero(self, tm , position):
+        if tm == 0:
+            print(tm, position)
+
+            self.env.step(-1, False)
+        if tm >= 2 and 0.45 > position > 0.30 and not self.env.stopped:
+            print(f"Position: {position}")
+            self.env.step(0, True)
+
+    def zeroangle(self, tm , position):
+        if tm == 0:
+            self.starting_position = position
+            self.env.step(-1, False)
+
+        if tm >= 2 and self.starting_position+self.movement_angle > position > self.starting_position+self.movement_angle-0.09:
+            print(f"Position: {position}")
+            self.env.step(0, True)
+
+    def zero(self, tm, position):
         delta = 0.2
-        self.last_time = time
+        self.last_time = tm
 
         direction = np.sign(self.goal - position) * self.coeff
+        if self.position is not None:
+            self.position_sum += np.abs(position - self.position)
 
         # if self.last_pos_2 == position:
         #     self.coeff *= self.coeff_decay
@@ -33,23 +68,27 @@ class Solver:
         self.last_pos_2 = self.last_pos
         self.last_pos = position
 
-        if self.delta is None:
-            self.delta = delta
-        else:
-            self.delta = self.delta * 0.3 + delta * 0.7
-
-        if self.position is not None:
-            self.speed = position - self.position
-            self.normalized_speed = self.speed / delta
-            real_speed = self.normalized_speed * self.delta
-            print(real_speed)
-            if position < self.goal < position + real_speed:
-                self.final_action = np.abs(self.goal - position) / real_speed
-            elif position > self.goal > position + self.speed:
-                self.final_action = np.abs(self.goal - position) / real_speed
-            else:
-                self.position = position
-            print(f"Position: {self.position}, Goal: {self.goal}, delta: {self.delta}, speed: {real_speed}, direction: {direction}")
+        self.delta = delta
+        # if self.delta is None:
+            # self.delta = delta
+        # else:
+            # self.delta = self.delt * 0.3 + delta * 0.7
+        if self.position is not None and self.position_sum>1:
+            if position>0.13:
+                self.final_action = 0
+            # self.speed = position - self.position
+            # self.normalized_speed = self.speed / delta
+            # real_speed = self.normalized_speed * self.delta
+            # real_speed = self.speed
+            # # print(real_speed)
+            # if position < self.goal < (position + real_speed):
+            #     self.final_action = np.abs(self.goal - position) / real_speed
+            # elif position > self.goal > (position + self.speed):
+            #     self.final_action = np.abs(self.goal - position) / real_speed
+            # else:
+            #     print("Stable position")
+            #     self.position = position
+            # print(f"Position: {self.position}, Goal: {self.goal}, delta: {self.delta}, speed: {real_speed}, direction: {direction}")
 
         else:
             self.position = position
