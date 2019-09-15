b4c12713b4ef64e49f496d00074161680505127e
Pagerous <arkadiuszpajor97@gmail.com>
2019-09-15 07:04:36 +0200

    added ci handler to server side

--- server/server.py
+++ server/server.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
+from command_interpreter import CommandInterpreter
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route("/",methods=['GET','OPTIONS'])
 def home():
+    ci = CommandInterpreter()
     data = request.args.get("command")
-    print(data)
-    return  render_template("index.html")
+    ci.interpreter(data)
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
